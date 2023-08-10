13eddb908eaad7872d4018b4407157805f78b236
Pharanya <pharanya_li@cmu.ac.th>
2023-08-11 00:10:03 +0700

    1.2

--- passenger05/README.md
+++ passenger05/README.md
@@ -1,4 +1,4 @@
-# vue-real-world
+# vue-lab03-step5
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/views/event/EventLayoutView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useEventStore } from '@/stores/event'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
+const store = useEventStore()
+const event = storeToRefs(store).event
+const id = ref(event?.value?.id)
+
+
+</script>
+
+<template>
+    <div>
+        <div v-if="event">
+            <h1>{{ event.title }}</h1>
+            <div id="nav">
+                <RouterLink :to="{ name: 'event-detail', params: { id } }">Details</RouterLink>
+                <RouterLink :to="{ name: 'event-register', params: { id } }">Register</RouterLink>
+                <RouterLink :to="{ name: 'event-edit', params: { id } }">Edit</RouterLink>
+            </div>
+            <RouterView :event="event"></RouterView>
+        </div>
+    </div>
+</template>
+
+
